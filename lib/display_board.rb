a14564098280802b050940c01e2d41e96eef4bcd
Muhamad Isa <djeruek4@gmail.com>
2019-01-07 12:22:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts "X | X | X"
+  puts " X | X | X "
   puts "-----------"
-  puts "X | X | X"
+  puts " X | X | X "
   puts "-----------"
-  puts "X | X | X"
+  puts " X | X | X "
 end
