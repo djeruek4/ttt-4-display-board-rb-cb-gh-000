2c901cf37c3127f4b652e51858579bb1c22e41b5
Muhamad Isa <djeruek4@gmail.com>
2019-01-07 12:39:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,4 @@ def display_board(board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"])
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 display_board()
